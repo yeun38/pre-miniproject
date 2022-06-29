9ce896ad242e708e60f90e389cdecd04b5c9af8e
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-06-29 22:50:17 +0900

    style join.html

--- assets/js/join.js
+++ assets/js/join.js
@@ -1,12 +1,17 @@
-function emailcheck() {
+function emailCheck() {
   const email_el = document.querySelector('input[name="email"]');
-  // const email_wrong = document.querySelector(".email-wrong");
+  const email_wrong = document.querySelector(".email-wrong");
   email_el.addEventListener("focusout", function () {
     const value = email_el.value;
     if (value.indexOf("@") == -1) {
-      alert("올바른 형식이 아닙니다.");
+      email_wrong.innerHTML="올바른 메일 형식이 아닙니다."
+      email_wrong.classList.remove('hidden')
     } else {
-      alert("사용할 수 있습니다.");
+      email_wrong.innerHTML="";
+      email_wrong.classList.add('hidden')
     }
   });
+}
+function pwCheck(){
+
 }
