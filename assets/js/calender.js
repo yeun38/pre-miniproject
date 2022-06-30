cd19d0542ee1d37f1af57af2912bf2f099510142
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-06-30 16:42:38 +0900

    달력수정

--- assets/js/calender.js
+++ assets/js/calender.js
@@ -1,3 +1,11 @@
 flatpickr("#date", {
     "locale": "ko"
 });
+flatpickr("#date_birth", {
+    "locale": "ko",
+    "defaultDate": "2001-01-01" 
+});
+const birth_el = document.querySelector('#date_birth');
+birth_el.addEventListener('focus', function(){
+    birth_el.classList.remove('empty-text')
+})
