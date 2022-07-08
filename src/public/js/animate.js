5148c5ba974fbe2f0b23d18d00bd708eae873d7d
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-07-08 20:56:11 +0900

    update /main

--- src/public/js/animate.js
+++ src/public/js/animate.js
@@ -1,36 +1,44 @@
-// let slider = document.querySelectorAll(".pic");
-// let slideContent = document.querySelectorAll(".pic + li");
-
-// slider = Array.from(slider)
-// slideContent = Array.from(slideContent)
-
-// slider.forEach((list, idx) => {
-//     list.addEventListener('click', () => {
-//         slideContent[idx].classList.toggle('active')
-//     })
-// })
-
-// 
+const wrap = document.querySelector('.wrap');
+// 헤더
 const header = document.querySelector('header');
 let h_top = header.offsetTop;
-const wrap = document.querySelector('.wrap');
-const weather = document.querySelector('#weather');
-let w_o_top = weather.scrollTop;
+// 웨더
+const weatherBar = document.querySelector('#weather');
+let weather_top = weatherBar.offsetTop;
 
 window.onscroll = function () {
     header_sticky()
 };
 
 function header_sticky() {
-    if (window.scrollY > h_top) {
-        if (!header.classList.contains('fixed')) {
-            header.classList.add('fixed')
-            wrap.style.paddingTop = header.clientHeight + 'px'
+    if (!weatherBar) {
+        if (window.scrollY > h_top) {
+            if (!header.classList.contains('fixed')) {
+                header.classList.add('fixed')
+                wrap.style.paddingTop = header.clientHeight + 'px'
+            }
+        } else {
+            if (header.classList.contains('fixed')) {
+                header.classList.remove('fixed')
+                wrap.style.paddingTop = '0px'
+            }
         }
     } else {
-        if (header.classList.contains('fixed')) {
-            header.classList.remove('fixed')
-            wrap.style.paddingTop = '0px'
+        if (window.scrollY > h_top + weather_top) {
+            if (!header.classList.contains('fixed')) {
+                header.classList.add('fixed')
+                weatherBar.classList.add('fixed')
+                weatherBar.style.top = header.clientHeight + "px";
+                wrap.style.paddingTop = header.clientHeight + weatherBar.clientHeight + 'px'
+            }
+        } else {
+            if (header.classList.contains('fixed')) {
+                header.classList.remove('fixed')
+                weatherBar.classList.remove('fixed')
+                weatherBar.style.top = "0px";
+                wrap.style.paddingTop = '0px'
+            }
         }
+
     }
 }
